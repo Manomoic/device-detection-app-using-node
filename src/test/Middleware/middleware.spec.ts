61a53587447129e848ec1a31a848e921aab61aca
jack_manamela <Jack.Manamela@derivco.co.za>
2023-07-04 08:11:47 +0200

    Updated the test suite

--- src/test/Middleware/middleware.spec.ts
+++ src/test/Middleware/middleware.spec.ts
@@ -1,6 +1,6 @@
 import DeviceDetector from "device-detector-js";
-import { normalizeDeviceInfo } from "../../Utilities/Utils";
 import { middleWare } from "../../Middleware/middleware";
+import { Request, Response } from "express";
 
 jest.mock("device-detector-js");
 jest.mock("../../Utilities/Utils");
@@ -13,49 +13,56 @@ let normalizeDeviceInfoMock = {
   normalizeDeviceInfo: jest.fn(),
 };
 
-describe("Middleware test suite", () => {
-  // Set up test data
-  let userAgent =
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3";
-  let device: ReturnType<DeviceDetector["parse"]> = {
-    client: {
-      type: "unknown",
-      name: "unknown",
-      version: "unknown",
-      engine: "unknown",
-      engineVersion: "unknown",
-    },
-    os: { name: "Windows", version: "10", platform: "x64" },
-    device: { type: "desktop", brand: "", model: "" },
-    bot: null,
+interface CustomRequest extends Request {
+  device?: any;
+  headers: {
+    [key: string]: string | undefined;
+    "user-agent"?: string;
   };
+}
 
-  beforeEach(() => {
-    deviceDetectorMock.deviceDetector.mockImplementation(() => userAgent);
-    normalizeDeviceInfoMock.normalizeDeviceInfo.mockReturnValue(device);
+describe("middleWare", () => {
+  const mockNext = jest.fn();
+  const mockRes = {} as Response;
+
+  afterEach(() => {
+    jest.clearAllMocks();
   });
 
-  it("should set req.device based on user-agent", () => {
-    const req: any = { headers: { "user-agent": userAgent } };
-    const res: any = {};
-    const next = jest.fn();
+  it("should set req.device with normalized device info when user agent is present", () => {
+    const mockReq: Partial<CustomRequest> = {
+      headers: {
+        "user-agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
+      },
+    };
 
-    const middlewareFunc = middleWare();
-    middlewareFunc(req, res, next);
+    const mockDevice = {
+      os: "Windows",
+      aspectRatio: "16:9",
+      osVersion: "10",
+      primaryHardwareType: "desktop",
+    };
 
-    expect(req.device).toEqual(device);
-    expect(next).toHaveBeenCalled();
+    DeviceDetector.prototype.parse = jest.fn().mockReturnValue(mockDevice);
+
+    middleWare()(mockReq as CustomRequest, mockRes, mockNext);
+
+    expect(DeviceDetector.prototype.parse).toHaveBeenCalledWith(
+      (mockReq.headers as { "user-agent": string })["user-agent"]
+    );
+    expect(mockReq.device).toEqual(mockDevice);
+    expect(mockNext).toHaveBeenCalled();
   });
 
-  it.skip("should set req.device to 'User agent not found' if user-agent is missing", () => {
-    const req: any = { headers: {} };
-    const res: any = {};
-    const next = jest.fn();
+  it("should set req.device with an error message when user agent is not present", () => {
+    const mockReq: Partial<CustomRequest> = {
+      headers: {},
+    };
 
-    const middlewareFunc = middleWare();
-    middlewareFunc(req, res, next);
+    middleWare()(mockReq as CustomRequest, mockRes, mockNext);
 
-    expect(req.device).toEqual({ message: "User agent not found" });
-    expect(next).toHaveBeenCalled();
+    expect(mockReq.device).toEqual({ message: "User agent not found" });
+    expect(mockNext).toHaveBeenCalled();
   });
 });
